0f9f994838ed74619501f25d5be0531b673da94a
Jaehyeon Park <skystar@skystar.dev>
2025-12-05 01:10:41 +0900

    refactor module

---
+++ nix/modules/kubix.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.kubix;
+
+  fetchableOptions = {
+    options = {
+      url = lib.mkOption {
+        type = lib.types.str;
+        description = "url to the crd file";
+      };
+      hash = lib.mkOption {
+        type = lib.types.str;
+        description = "hash of the crd file";
+      };
+    };
+  };
+
+  validatorLib = import ../lib/validator.nix { inherit pkgs lib config; };
+in
+{
+  options.kubix = {
+    enable = lib.mkEnableOption "Enable kubix module";
+
+    crds = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule fetchableOptions);
+      description = "list of crds to fetch and include";
+      default = [ ];
+    };
+
+    schemas = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule fetchableOptions);
+      description = "list of json schemas to fetch and include";
+      default = [ ];
+    };
+
+    manifests = lib.mkOption {
+      type = lib.types.attrsOf lib.types.attrs;
+      description = "manifests definition";
+    };
+  };
+
+  config = {
+    kubix = lib.mkIf cfg.enable {
+      result = "TODO";
+    };
+  };
+}
