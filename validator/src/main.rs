8cb910cc1bd20be6d54dd391cc7c1a2f13b02559
Jaehyeon Park <skystar@skystar.dev>
2025-12-04 20:47:58 +0900

    parse arg

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+#[derive(argh::FromArgs)]
+/// kubix validator
+struct Arguments {
+    /// directory containing CRD files, in json format
+    #[argh(option, short = 'c')]
+    crd_dir: PathBuf,
+
+    /// directory containing resource manifest files to validate, in json format
+    #[argh(option, short = 'm')]
+    manifest_dir: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: Arguments = argh::from_env();
+
+    Ok(())
 }
