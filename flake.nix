5683593d98b32e71c571e724af852bafc7e6eaaa
Jaehyeon Park <skystar@skystar.dev>
2025-12-04 16:55:10 +0900

    add flake skeleton

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "kubix, the Kubernetes manifest generator";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+        };
+    };
+}
